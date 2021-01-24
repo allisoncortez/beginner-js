f1b866ccb0a686e009d2a070b142f6821a6595f6
allisoncortez <allizetorc@gmail.com>
2021-01-24 11:59:25 -0600

    grab image source

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -3,7 +3,11 @@ const cardButtons = document.querySelectorAll('.card button');
 function handleCardButtonClick(e) {
     const button = e.currentTarget;
     const card = button.closest('.card');
-    console.log(card);
+    //grab img source
+    const imgSrc = card.querySelector('img').src;
+    const desc = card.dataset.description;
+
+    console.log(desc);
 }
 
 cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick))
