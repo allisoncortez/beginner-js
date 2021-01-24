f171db3269405027962291124630596f2686eaa1
allisoncortez <allizetorc@gmail.com>
2021-01-24 11:53:26 -0600

    test card variable inside handleCardButtonClick function

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,7 +1,9 @@
 const cardButtons = document.querySelectorAll('.card button');
 
-function handleCardButtonClick() {
-    console.log('you clicked the button!')
+function handleCardButtonClick(e) {
+    const button = e.currentTarget;
+    const card = button.closest('.card');
+    console.log(card);
 }
 
 cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick))
